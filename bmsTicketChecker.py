33c11f4d741319525d55bc4b7575473bf06954fc
Ankit Goyal <ankitgoyal.bpl@gmail.com>
2015-02-28 05:00:24 +0530

    Select Movie Addition

--- bmsTicketChecker.py
+++ bmsTicketChecker.py
@@ -8,7 +8,8 @@ from BeautifulSoup import BeautifulSoup, SoupStrainer
 from collections import defaultdict
 
 # Global Constants
-ERROR_MSG = "Some Error while contacting WebSite. Please Try again later"
+WEBSITE_ERROR_MSG = "Some Error while contacting WebSite. Please Try again later"
+INPUT_ERROR_MSG = "Wrong Input entered. Exiting"
 BMS_WEBSITE_ADDRESS = "http://in.bookmyshow.com"
 BMS_CITY_NAME = "hyderabad"
 BMS_MOVIES_WEBPAGE = "movies"
@@ -27,6 +28,7 @@ def GetMoviesList() :
       if len(movieSoup) < 1 :
             return None
 
+      # TODO: Make sure that we need a dictionary over here and not list. If not convert this to a list of tuples
       movieDict = defaultdict(list)
       for movieData in movieSoup :
             movieDict[movieData["data"]].append(movieData["href"])
@@ -34,6 +36,24 @@ def GetMoviesList() :
 
       return movieDict
 
+# Function to ask for User Input to select a movie out of list
+# of movies in his/her city
+def SelectMovie(movieDict) :      
+      movieCouner = 1
+      for movieItem in movieDict.values() :
+            print movieCouner, movieItem[1]
+            movieCouner = movieCouner + 1
+
+      inputIndex = raw_input("Please Enter your Movie Choice: ")
+      try :
+            choosenIndex = int(inputIndex) - 1
+      except : 
+            print INPUT_ERROR_MSG
+
+      return movieDict.values()[choosenIndex]
+
 
 movieDict = GetMoviesList()
-print len(movieDict)
+(movieLink, movieTitle) = SelectMovie(movieDict)
+
+print "Choosen Movie", movieLink, movieTitle
